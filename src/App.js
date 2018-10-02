4261967c0230e641f115988ddeb69ed073294876
Gokhan Maden <gokhanmaden1@gmail.com>
2018-10-03 02:07:00 +0300

    slider is DONE

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import "./resources/styles.css";
 
-import Header from "./components/Header_Footer/Header";
+import Header from "./components/Header-Footer/Header";
+import Featured from "./components/Featured/";
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height:"1500px" , background: "cornflowerblue"}}>
         <Header />
+        <Featured />
       </div>
     );
   }
